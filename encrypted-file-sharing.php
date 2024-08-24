4ed0e3d788f578cb563c77c0928993efd3bab660
Lance Desk <lncedesk@gmail.com>
2024-08-24 17:32:33 +0300

    Initial plugin header setup.

---
+++ encrypted-file-sharing.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name:     Encrypted File Sharing
+ * Plugin URI:      https://github.com/lancedesk/encrypted-file-sharing
+ * Description:     A plugin that allows site administrators to securely send files to specific users via the WordPress admin panel.
+ * Author:          Robert June
+ * Author URI:      https://profiles.wordpress.org/lancedesk/
+ * Text Domain:     encrypted-file-sharing
+ * Domain Path:     /languages
+ * Version:         1.0.0
+ *
+ * @package         encrypted-file-sharing
+ */
+
+/* Ensure that WordPress functions are available */
+if (!defined('ABSPATH')) 
+{
+    exit; /* Exit if accessed directly */
+}
