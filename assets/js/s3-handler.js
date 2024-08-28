c74276e51c4f2423d485b4cef6b9b516beaf67c2
Lance Desk <lncedesk@gmail.com>
2024-08-28 16:05:13 +0300

    Added js Ajax script to s3 handler to handle async S3 bucket creation.

--- assets/js/s3-handler.js
+++ assets/js/s3-handler.js
@@ -1,4 +1,49 @@
 jQuery(document).ready(function ($) {
+    /* Handle bucket creation */
+    $('#efs_create_bucket').on('click', function() {
+        var newBucket = $('#efs_new_bucket').val();
+        var region = $('#efs_aws_region').val();
+        var accessKey = $('#efs_aws_access_key').val();
+        var secretKey = $('#efs_aws_secret_key').val();
+
+        if (newBucket === '') {
+            $('#efs_create_bucket_message').html('<span style="color: red;">Please enter a bucket name.</span>');
+            return;
+        }
+
+        /* Ensure AWS credentials are provided */
+        if (!region || !accessKey || !secretKey) {
+            alert('Please provide AWS region, access key, and secret key.');
+            return;
+        }
+
+        $.ajax({
+            url: efsAdminAjax.ajax_url, /* WordPress admin AJAX URL */
+            type: 'POST',
+            data: {
+                action: 'efs_create_s3_bucket',
+                bucket_name: newBucket,
+                region: region,
+                access_key: accessKey,
+                secret_key: secretKey,
+                _ajax_nonce: efs_s3_params.efs_s3_nonce  /* Include nonce for security */
+            },
+            success: function(response) {
+                if (response.success) {
+                    $('#efs_create_bucket_message').html('<span style="color: green;">Bucket created successfully!</span>');
+                    $('#efs_new_bucket').val(''); /* Clear input field */
+                    fetchBuckets(); /* Fetch updated list of buckets */
+                } else {
+                    $('#efs_create_bucket_message').html('<span style="color: red;">' + response.data.message + '</span>');
+                }
+            },
+            error: function() {
+                $('#efs_create_bucket_message').html('<span style="color: red;">Failed to create bucket. Please try again.</span>');
+            }
+        });
+    });
+
+
     $('#efs_fetch_buckets').on('click', function () {
         var region = $('#efs_aws_region').val();
         var accessKey = $('#efs_aws_access_key').val();
