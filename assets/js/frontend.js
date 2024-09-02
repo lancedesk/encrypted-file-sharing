4f9816576f473126b38dafdcd3211fa2a0965f86
Lance Desk <lncedesk@gmail.com>
2024-09-02 17:44:26 +0300

    Since files are now being served from the server as part of headers, I have switched from Ajax to HTTP request for download which has worked.

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -1,29 +1,30 @@
 jQuery(document).ready(function($) {
-    /* Handle click event for decrypt / download button */
+    /* Handle click event for decrypt/download button */
     $('.download-btn').on('click', function(e) {
         e.preventDefault();
         var fileId = $(this).data('file-id');
 
-        $.ajax({
-            url: efsAdminAjax.ajax_url, /* Use localized variable for AJAX URL */
-            type: 'POST',
-            data: {
-                action: 'efs_handle_download',
-                file_id: fileId,
-                security: efsAdminAjax.nonce /* Use the global variable for security nonce */
-            },
-            success: function(response) {
-                if (response.success) {
-                    /* The file download will be triggered automatically by PHP */
-                    alert('Download started.');
-                } else {
-                    alert(response.data.message);  /* Show error if decryption/download fails */
-                }
-            },
-            error: function() {
-                alert('Failed to download file. Please try again.');
-            }
-        });
-    });
+        /* Create a form to submit the download request */
+        var downloadForm = $('<form>', {
+            action: efsAdminAjax.ajax_url, /* Use localized variable for the AJAX URL */
+            method: 'POST',
+            style: 'display: none'
+        }).append($('<input>', {
+            type: 'hidden',
+            name: 'action',
+            value: 'efs_handle_download'
+        })).append($('<input>', {
+            type: 'hidden',
+            name: 'file_id',
+            value: fileId
+        })).append($('<input>', {
+            type: 'hidden',
+            name: 'security',
+            value: efsAdminAjax.nonce /* Use the global variable for the security nonce */
+        }));
 
+        /* Append form to body and submit */
+        $('body').append(downloadForm);
+        downloadForm.submit();
+    });
 });
